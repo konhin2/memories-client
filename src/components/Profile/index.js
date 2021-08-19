92f333d5e32d9e711a5ca830f04bc921783678b7
Konhin CM <maskmesta@gmail.com>
2021-08-19 18:19:53 -0500

    done

--- src/components/Profile/index.js
+++ src/components/Profile/index.js
@@ -106,7 +106,7 @@ export default function Profile() {
                                                 <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                             </svg>
                                             <div className="flex text-sm text-gray-600">
-                                                <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-memory-c5 hover:text-memory-c4">
+                                                <label htmlFor="file-upload" className="relative cursor-pointer rounded-md font-medium text-memory-c5 hover:text-memory-c4">
                                                     <span>Upload a file</span>
                                                     <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={e => uploadImage(e)} />
                                                 </label>
@@ -125,7 +125,7 @@ export default function Profile() {
                                                 <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
                                             </svg>
                                             <div className="flex text-sm text-gray-600">
-                                                <label htmlFor="file-upload" className="relative cursor-pointer bg-white rounded-md font-medium text-memory-c5 hover:text-memory-c4">
+                                                <label htmlFor="file-upload" className="relative cursor-pointer rounded-md font-medium text-memory-c5 hover:text-memory-c4">
                                                     <span>Upload a file</span>
                                                     <input id="file-upload" name="file-upload" type="file" className="sr-only" onChange={e => uploadImage(e)} />
                                                 </label>
